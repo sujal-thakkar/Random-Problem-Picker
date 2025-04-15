dc856eed7890803d9610c65c04203a6ce23b9c32
Sujal Thakkar <sujalthakkar153@gmail.com>
2025-04-15 14:03:54 +0530

    Add new coding questions to the question bank, organized by topic

--- script.js
+++ script.js
@@ -1,24 +1,145 @@
 const questions = [
+    // Arrays & Hashing:-
     {
-        topic: "Sliding Window",
-        title: "Longest Substring Without Repeating Characters",
-        link: "https://leetcode.com/problems/longest-substring-without-repeating-characters/"
+        title: "Contains Duplicate",
+        link: "https://leetcode.com/problems/contains-duplicate/"
     },
     {
-        topic: "Two Pointers",
-        title: "3Sum",
-        link: "https://leetcode.com/problems/3sum/"
+        title: "Valid Anagram",
+        link: "https://leetcode.com/problems/valid-anagram/"
     },
     {
-        topic: "Hashing",
         title: "Two Sum",
         link: "https://leetcode.com/problems/two-sum/"
     },
     {
-        topic: "Binary Search",
-        title: "Find Minimum in Rotated Sorted Array",
+        title: "Group Anagrams",
+        link: "https://leetcode.com/problems/group-anagrams/"
+    },
+    {
+        title: "Top K Frequent Elements",
+        link: "https://leetcode.com/problems/top-k-frequent-elements/"
+    },
+    {
+        title: "Encode and Decode Strings",
+        link: "https://neetcode.io/problems/string-encode-and-decode/"
+    },
+    {
+        title: "Product of Array Except Self",
+        link: "https://leetcode.com/problems/product-of-array-except-self/"
+    },
+    {
+        title: "Valid Sudoku",
+        link: "https://leetcode.com/problems/valid-sudoku/"
+    },
+    {
+        title: "Longest Consecutive Sequence",
+        link: "https://leetcode.com/problems/longest-consecutive-sequence/"
+    },
+    // Two Pointers:-
+    {
+        title: "Valid Palindrome",
+        link: "https://leetcode.com/problems/valid-palindrome/"
+    },
+    {
+        title: "Two Sum II Input Array Is Sorted",
+        link: "https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/"
+    },
+    {
+        title: "3Sum",
+        link: "https://leetcode.com/problems/3sum/"
+    },
+    {
+        title: "Container With Most Water",
+        link: "https://leetcode.com/problems/container-with-most-water/"
+    },
+    {
+        title: "Trapping Rain Water",
+        link: "https://leetcode.com/problems/trapping-rain-water/"
+    },
+    // Sliding Window:-
+    {
+        title: "Best Time to Buy And Sell Stock",
+        link: "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/"
+    },
+    {
+        title: "Longest Substring Without Repeating Characters",
+        link: "https://leetcode.com/problems/longest-substring-without-repeating-characters/"
+    },
+    {
+        title: "Longest Repeating Character Replacement",
+        link: "https://leetcode.com/problems/longest-repeating-character-replacement/"
+    },
+    {
+        title: "Permutation In String",
+        link: "https://leetcode.com/problems/permutation-in-string/"
+    },
+    {
+        title: "Minimum Window Substring",
+        link: "https://leetcode.com/problems/minimum-window-substring/"
+    },
+    {
+        title: "Sliding Window Maximum",
+        link: "https://leetcode.com/problems/sliding-window-maximum/"
+    },
+    // Stack:-
+    {
+        title: "Valid Parentheses",
+        link: "https://leetcode.com/problems/valid-parentheses/"
+    },
+    {
+        title: "Min Stack",
+        link: "https://leetcode.com/problems/min-stack/"
+    },
+    {
+        title: "Evaluate Reverse Polish Notation",
+        link: "https://leetcode.com/problems/evaluate-reverse-polish-notation/"
+    },
+    {
+        title: "Generate Parentheses",
+        link: "https://leetcode.com/problems/generate-parentheses/"
+    },
+    {
+        title: "Daily Temperatures",
+        link: "https://leetcode.com/problems/daily-temperatures/"
+    },
+    {
+        title: "Car Fleet",
+        link: "https://leetcode.com/problems/car-fleet/"
+    },
+    {
+        title: "Largest Rectangle In Histogram",
+        link: "https://leetcode.com/problems/largest-rectangle-in-histogram/"
+    },
+    // Binary Search:-
+    {
+        title: "Binary Search",
+        link: "https://leetcode.com/problems/binary-search/"
+    },
+    {
+        title: "Search a 2D Matrix",
+        link: "https://leetcode.com/problems/search-a-2d-matrix/"
+    },
+    {
+        title: "Koko Eating Bananas",
+        link: "https://leetcode.com/problems/koko-eating-bananas/"
+    },
+    {
+        title: "Find Minimum In Rotated Sorted Array",
         link: "https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/"
-    }
+    },
+    {
+        title: "Search In Rotated Sorted Array",
+        link: "https://leetcode.com/problems/search-in-rotated-sorted-array/"
+    },
+    {
+        title: "Time Based Key Value Store",
+        link: "https://leetcode.com/problems/time-based-key-value-store/"
+    },
+    {
+        title: "Median of Two Sorted Arrays",
+        link: "https://leetcode.com/problems/median-of-two-sorted-arrays/"
+    },
 ];
 
 let completedQuestions = JSON.parse(localStorage.getItem('completedQuestions')) || [];
